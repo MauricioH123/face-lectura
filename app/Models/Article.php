02b8e28b38fda53a3f6799de9c166390537fd7b2
MauricioH <mauricioh12345678912@gmail.com>
2025-01-30 17:39:48 -0500

    migraciones y modelos base

---
+++ app/Models/Article.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Article extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'author',
+        'description',
+        'pages',
+        'genre_id',
+        'clasification_id',
+    ];
+}
--- database/migrations/2025_01_30_221221_create_articles_table.php
+++ database/migrations/2025_01_30_221221_create_articles_table.php
@@ -11,15 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('role_id');
-            $table->softDeletes();
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('description');
+            $table->longText('content');
+            $table->string('pdf');
+            $table->integer('pages');
+            $table->integer('genre_id');
+            $table->integer('clasification_id');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 };
